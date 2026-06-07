b29ec0f6724d4eff6c5de35eccc451280e000f90
PocokpoWer <farkas.tibor1105@gmail.com>
2026-06-07 06:45:16 +0200

    First commit

---
+++ hsm-backend/src/test/java/com/example/hsmbackend/HsmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hsmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
