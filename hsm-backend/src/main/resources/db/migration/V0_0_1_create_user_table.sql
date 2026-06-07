a930f7135ee3939a828b95ef5e94d5119180ff1b
PocokpoWer <farkas.tibor1105@gmail.com>
2026-06-07 14:57:06 +0200

    connect to mysql with docker,flyway + dependecies

---
+++ hsm-backend/src/main/resources/db/migration/V0_0_1_create_user_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `social_media_database`.`users`
+(
+    `id`         INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(100) NULL,
+    `last_name`  VARCHAR(100) NULL,
+    `birth_date` DATE NULL,
+    `email`      VARCHAR(100) NULL,
+    `password`   VARCHAR(100) NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
